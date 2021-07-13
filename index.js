1752d5068ff7194388d49da9adfa7855e0cefff7
Sotatek-KhangTrinh <khang.trinh@sotatek.com>
2021-07-13 14:59:32 +0700

    fix tam cung block

--- index.js
+++ index.js
@@ -2,24 +2,44 @@ const abi = require("./poolAbi.json");
 var Web3 = require("web3");
 
 // "Web3.providers.givenProvider" will be set if in an Ethereum supported browser.
-
+const divide = (start, end, step) => {
+    const arr = [];
+    if (end - start < step) return [{ start, end }];
+    for (let i = start + step; i <= end; i += step + 1) {
+      arr.push({ start: i - step, end: i });
+      if (i + step + 1 > end && i < end) {
+        arr.push({ start: i + 1, end: end });
+      }
+    }
+    return arr;
+  };
+  
 (async () => {
   try {
     var web3 = new Web3(
-      "https://data-seed-prebsc-1-s3.binance.org:8545"
+      "https://mainnet.infura.io/v3/710b741fe9924cc8a5fa4fa20a89e620"
     );
-
+    const startBlock = 12534247;
+    const lastestBlock = 12817416;
     // 1 Pool vi du
     const contract = new web3.eth.Contract(
       abi,
-      "0xD7de2af41a61387750C19Ac1BFd599d722c2c663"
+      "0x7858e59e0c01ea06df3af3d20ac7b0003275d4bf"
     );
-    const swapHistory = await contract.getPastEvents("Swapped", {
-        fromBlock: 'earliest',
-        toBlock: 'latest',
-    });
-    swapHistory.forEach(element => {
-        console.log(element.returnValues)
+    const rangeBlock = divide(startBlock, lastestBlock, 10000);
+    const listTrans = [].concat.apply(
+      [],
+      await Promise.all(
+        rangeBlock.map((block) => {
+          return contract.getPastEvents("Swap", {
+            fromBlock: block.start,
+            toBlock: block.end,
+          });
+        })
+      )
+    );
+    listTrans.forEach((element) => {
+      console.log(element.returnValues);
     });
 
     // pool khasc thi tao them 1 contract, khac moi dia chi
@@ -27,3 +47,4 @@ var Web3 = require("web3");
     console.log(e);
   }
 })();
+
